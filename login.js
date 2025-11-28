b96514f077e6d5b2fbc98d15a02ec637099c6dcf
Palaksingh0 <164669321+Palaksingh0@users.noreply.github.com>
2025-11-28 22:42:48 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,24 @@
+/*function loginUser() {
+    let savedUser = JSON.parse(localStorage.getItem("userData"));
+
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    if (!savedUser) {
+        alert("No user registered!");
+        return;
+    }
+
+    if (username === savedUser.username && password === savedUser.password) {
+        alert("Login Successful!");
+
+        // Show the course button
+        document.getElementById("courseBtn").style.display = "block";
+
+    } else {
+        alert("Incorrect Username or Password!");
+    }
+}*/
+console.log("Login page loaded");
+
+
